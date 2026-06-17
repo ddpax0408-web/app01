92a6a59ea9e0938629e6b2149265fd7939131c9b
ddpax0408-web <ddpax0408@gmail.com>
2026-06-17 12:10:57 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // (CSS 파일이 있다면)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
